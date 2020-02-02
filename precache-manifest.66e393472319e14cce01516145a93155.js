2044338ca5914d24ea602ca9d2acd0cfbeb7275b
LongBody <nguyenthanhlong1120@gmail.com>
2020-02-03 01:52:36 +0700

    Updates

---
+++ precache-manifest.66e393472319e14cce01516145a93155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695f57d9ba637a95e7f42a9c185dd7ec",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "49b4629414703c5e4801",
+    "url": "/react/static/css/main.028dd7a4.chunk.css"
+  },
+  {
+    "revision": "979b28d586565004043f",
+    "url": "/react/static/js/2.cceb7cc3.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react/static/js/2.cceb7cc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49b4629414703c5e4801",
+    "url": "/react/static/js/main.4a492e84.chunk.js"
+  },
+  {
+    "revision": "c7475efd13b862b6a42f",
+    "url": "/react/static/js/runtime-main.2675b949.js"
+  }
+]);
